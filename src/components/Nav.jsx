f9a03bd56ad4b7dd5a0010e1c2ab6680f7563212
AJ <ajthraen@gmail.com>
2022-09-28 21:04:57 -0400

    nav complete

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -16,7 +16,7 @@ const Nav = () => {
             </a>
           </li>
           <li className="nav__list">
-            <a href="/" className="nav__link">
+            <a href="/books" className="nav__link">
               Books
             </a>
           </li>
@@ -34,6 +34,23 @@ const Nav = () => {
           <button className="btn__menu btn__menu--close">
             <FontAwesomeIcon icon="times" />
           </button>
+          <ul className="menu__links">
+            <li className="menu__list">
+              <a href="/home" className='menu__link'>
+                Home
+              </a>
+            </li>
+            <li className="menu__list">
+              <a href="/books" className='menu__link'>
+                Books
+              </a>
+            </li>
+            <li className="menu__list">
+              <a href="/cart" className='menu__link'>
+                Cart
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
     </nav>

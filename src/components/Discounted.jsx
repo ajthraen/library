5fb0b09e304a97c3c8642072488711af93af2132
AJ <ajthraen@gmail.com>
2022-10-02 18:12:36 -0400

    explore

--- src/components/Discounted.jsx
+++ src/components/Discounted.jsx
@@ -12,7 +12,10 @@ const Discounted = () => {
                         <h2>Discount <span className="purple">Books</span></h2>
                     </div>
                     <div className="books">
-                        {books.map((book) => (
+                        {books
+                            .filter(book => book.salePrice > 0)
+                            .slice(0, 8)
+                            .map((book) => (
                             <Book book={book} key={book.id} />
                         ))}
                     </div>

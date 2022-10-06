a2e9129fb2b02bb7df9038681bb0e07f7c137bce
AJ <ajthraen@gmail.com>
2022-10-05 21:12:04 -0400

    dynamic price1

--- src/pages/BookInfo.jsx
+++ src/pages/BookInfo.jsx
@@ -1,11 +1,13 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Price from '../components/Price';
+import Rating from '../components/Rating';
 
 const BookInfo = ({books}) => {
     return (
         <div id="books__body">
-            <div className="books__main">
+            <main id="books__main">
                 <div className="books__container">
                     <div className="row">
                         <div className="books__selected--top">
@@ -21,13 +23,39 @@ const BookInfo = ({books}) => {
                                 </figure>
                                 <div className="book__selected--description">
                                     <h2 className="book__selected--title">Crack</h2>
-                                    <FontAwesomeIcon icon="star" />
+                                    <Rating rating="4.5" />
+                                    <div className="book_selected--price">
+                                        <Price originalPrice={13} salePrice={10} />
+                                    </div>
+                                    <div className="book__summary">
+                                        <div className="book_summary--title">
+                                            <h2>Summary</h2>
+                                        </div>
+                                        <div className="book__summary--para">
+                                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto aperiam error molestias sunt at recusandae iusto omnis dolor ad dolorum? Praesentium nihil ratione maxime? Voluptatem molestias ea praesentium vitae recusandae!</p>
+                                        </div>
+                                        <div className="book__summary--para">
+                                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto aperiam error molestias sunt at recusandae iusto omnis dolor ad dolorum? Praesentium nihil ratione maxime? Voluptatem molestias ea praesentium vitae recusandae!</p>
+                                        </div>
+                                        <button className="btn">
+                                            Add to Cart
+                                        </button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
+                <div className="books__container">
+                    <div className="row">
+                        <div className="book__selected--top">
+                            <h2 className="book__selected--title--top">
+                                Recommended Books
+                            </h2>
+                        </div>
+                    </div>
+                </div>
+            </main>
         </div>
     );
 }
